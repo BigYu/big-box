656b9c74968c2c258008c58ce43b21950d72b146
BigYu <jinyucong911@outlook.com>
2017-02-08 23:04:09 +0800

    refine animation

--- src/ui/src/index.js
+++ src/ui/src/index.js
@@ -2,16 +2,16 @@ const $ = require('zeptojs');
 const iSlider = require('islider.js')
 
 let list = [
-  { content: './static/images/1.jpg' },
+  { content: '<div><h1>Page1</h1><h2>This is page1</h2><p>page1 is pretty awsome</p><div>' },
   { content: './static/images/2.jpg' },
-  { content: './static/images/3.jpg' },
+  { content: '<div><h1>Page1</h1><h2>This is page1</h2><p>page1 is pretty awsome</p><div>' },
 ]
 
 let islider = new iSlider(document.getElementById("iSlider-wrapper"), list, {
-    isAutoplay: 1,
+    isAutoplay: 0,
     isLooping: 0,
     isOverspread: 1,
     animateTime: 800,
-    isVertical: true,
-    animateType: 'rotate'
+    isVertical: 0,
+    animateType: 'flow'
 });
