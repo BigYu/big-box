cf58c210b205c9b7ef00709add993abb3547dede
BigYu <jinyucong911@outlook.com>
2017-02-08 19:14:55 +0800

    update default route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use('/static', express.static(__dirname + '/ui'));
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/ui/index.html'));
+});
 
 const server = app.listen(80);
